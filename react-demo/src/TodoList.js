cc03c544507b27c013fbf0dba40b77638e05ec40
ChenZhiPeng <634323580@qq.com>
2020-11-09 18:08:04 +0800

    fix

--- react-demo/src/TodoList.js
+++ react-demo/src/TodoList.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import { Input, Button, List } from 'antd';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import store from './store/index'
-import { getInputChangeAction, getBtnClickAction, getItemDeleteAction } from './store/actionCreators'
+import { getInputChangeAction, getBtnClickAction, getItemDeleteAction, getTodoList } from './store/actionCreators'
+import TodoListUI from './TodoListUI'
 
 
 class TodoList extends Component {
@@ -14,35 +14,25 @@ class TodoList extends Component {
     this.handlerInputChange = this.handlerInputChange.bind(this)
     this.handlerStoreChange = this.handlerStoreChange.bind(this)
     this.handlerBtnClick = this.handlerBtnClick.bind(this)
+    this.handlerItemDelete = this.handlerItemDelete.bind(this)
     // 监听store改变
     store.subscribe(this.handlerStoreChange)
   }
 
   render() {
     return (
-      <div style={{marginTop: '10px', marginLeft: '10px'}}>
-        <div>
-          <Input
-            value={this.state.inputValue}
-            placeholder="todo info"
-            style={{width: '300px', marginRight: '10px'}}
-            onChange={this.handlerInputChange}/>
-          <Button
-            onClick={this.handlerBtnClick}
-            type="primary">提交</Button>
-        </div>
-        <List
-          style={{marginTop: '10px', width: '300px'}}
-          bordered
-          dataSource={this.state.list}
-          renderItem={(item, index) => (
-            <List.Item onClick={this.handlerItemDelete.bind(this, index)}>
-              {item}
-            </List.Item>
-          )}
-        />
-      </div>
+      <TodoListUI
+        inputValue={this.state.inputValue}
+        list={this.state.list}
+        handlerInputChange={this.handlerInputChange}
+        handlerBtnClick={this.handlerBtnClick}
+        handlerItemDelete={this.handlerItemDelete}
+      />
     )
+  }
+  componentDidMount() {
+    const action = getTodoList()
+    store.dispatch(action)
   }
   // 输入框改变
   handlerInputChange(e) {
--- react-demo/src/TodoListUI.js
+++ react-demo/src/TodoListUI.js
@@ -1,1 +1,60 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Input, Button, List } from 'antd';
+
+// 无状态组件
+const TodoListUI = (props) => {
+  return (
+      <div style={{marginTop: '10px', marginLeft: '10px'}}>
+      <div>
+        <Input
+          value={props.inputValue}
+          placeholder="todo info"
+          style={{width: '300px', marginRight: '10px'}}
+          onChange={props.handlerInputChange}/>
+        <Button
+          onClick={props.handlerBtnClick}
+          type="primary">提交</Button>
+      </div>
+      <List
+        style={{marginTop: '10px', width: '300px'}}
+        bordered
+        dataSource={props.list}
+        renderItem={(item, index) => (
+          <List.Item onClick={() => {props.handlerItemDelete(index)}}>
+            {item}
+          </List.Item>
+        )}
+      />
+    </div>
+  )
+}
+
+// class TodoListUI extends Component {
+//   render() {
+//     return (
+//       <div style={{marginTop: '10px', marginLeft: '10px'}}>
+//         <div>
+//           <Input
+//             value={this.props.inputValue}
+//             placeholder="todo info"
+//             style={{width: '300px', marginRight: '10px'}}
+//             onChange={this.props.handlerInputChange}/>
+//           <Button
+//             onClick={this.props.handlerBtnClick}
+//             type="primary">提交</Button>
+//         </div>
+//         <List
+//           style={{marginTop: '10px', width: '300px'}}
+//           bordered
+//           dataSource={this.props.list}
+//           renderItem={(item, index) => (
+//             <List.Item onClick={() => {this.props.handlerItemDelete(index)}}>
+//               {item}
+//             </List.Item>
+//           )}
+//         />
+//       </div>
+//     )
+//   }
+// }
+export default TodoListUI
